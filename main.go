7c4501b3f00778e5986ef239c93e0decb3abfe9e
marcin <asssinowo90@gmail.com>
2023-10-26 22:48:28 +0200

    Revert "first release"
    This reverts commit 2ca3d0680d763e74d01ab893cbbab6e88a2cc8d7.

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
 )
 
+func read(r http.ResponseWriter, r2 *http.Request) {
+	f, err := exec.Command("journalctl").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(r, string(f))
+	log.Print("Returned log file to browser!")
+}
 func main() {
-	app := tview.NewApplication()
-	list := tview.NewList()
-	for _, s := range list_services() {
-		list.AddItem(s, "", 1, nil)
-	}
-	if err := app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/", read)
+	http.ListenAndServe(":8080", nil)
 }
